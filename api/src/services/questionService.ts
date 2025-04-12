cbf8d5411e3d253b459412c7e78985f8f1f6c066
ashwin <ashwinbose6@gmail.com>
2025-04-12 08:30:13 +0100

    fine tuned embedding model via linear adapater

--- api/src/services/questionService.ts
+++ api/src/services/questionService.ts
@@ -1,8 +1,19 @@
 import { QuestionModel } from "../models/question";
+import axios from "axios";
+
+const embedText = async (text: string) => {
+    const response = await axios.post("localhost:5000", {
+        text
+    });
+
+    const { embedding } = response.data;
+    return embedding;
+}
 
 class QuestionService {
     static async addQuestion(question: string, options: [string], topics: [string], answer: number): Promise<void> {
-        const newQuestion = new QuestionModel({ question, options, topics, answer });
+        const embedding = await embedText(question);
+        const newQuestion = new QuestionModel({ question, options, topics, answer, embedding });
         await newQuestion.save();
     }
 }
