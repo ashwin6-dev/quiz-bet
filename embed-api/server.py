2f1439d04492e70890c851fa9704e17312dbe6e9
ashwin <ashwinbose6@gmail.com>
2025-04-12 14:49:01 +0100

    added option is embed rest api to use the adapter

--- embed-api/server.py
+++ embed-api/server.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List
 from sentence_transformers import SentenceTransformer
+import torch
 
 app = FastAPI()
 
@@ -11,8 +12,11 @@ try:
 except Exception as e:
     raise RuntimeError(f"Error loading embedding model '{model_name}': {e}")
 
+adapter = torch.jit.load("../models/embed-finetuning/adapter.pt")
+
 class EmbedRequest(BaseModel):
     text: str
+    useAdapter: bool
 
 class EmbedResponse(BaseModel):
     text: str
@@ -21,8 +25,17 @@ class EmbedResponse(BaseModel):
 @app.post("/embed", response_model=EmbedResponse)
 async def embed_text(request_data: EmbedRequest):
     text = request_data.text
+    use_adapter = request_data.useAdapter
+
     try:
-        embedding = model.encode(text).tolist()
+        embedding = model.encode(text)
+
+        if use_adapter:
+            embedding_tensor = torch.from_numpy(embedding)
+            embedding = adapter(embedding_tensor)
+
+        embedding = embedding.tolist()
+
         return EmbedResponse(text=text, embedding=embedding)
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Error generating embedding: {e}")
